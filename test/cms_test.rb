add389ac23eedbf739db7185f2e55c4d49cb45f5
Kramer Keller <kramerkeller@gmail.com>
2018-07-25 16:22:35 -0400

    access session during testing

--- test/cms_test.rb
+++ test/cms_test.rb
@@ -30,6 +30,10 @@ class CmsTest < Minitest::Test
     end
   end
 
+  def session
+    last_request.env["rack.session"]
+  end
+
   def test_index
     create_document "about.md"
     create_document "changes.txt"
@@ -51,14 +55,11 @@ class CmsTest < Minitest::Test
     assert_equal "text/plain", last_response["Content-Type"]
   end
 
-  def test_non_existent_file
+  def test_document_not_found
     get "/notafile.ext"
+
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_equal 200, last_response.status
-    assert_includes last_response.body, "notafile.ext does not exist"
+    assert_equal "notafile.ext does not exist", session[:error]
   end
 
   def test_viewing_markdown_file
@@ -84,12 +85,8 @@ class CmsTest < Minitest::Test
   # Updating document
   def test_updating_document
     post "/changes.txt", contents: "new content"
-
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-
-    assert_includes last_response.body, "changes.txt has been updated"
+    assert_equal "changes.txt has been updated", session[:success]
 
     get "/changes.txt"
     assert_equal 200, last_response.status
@@ -98,7 +95,6 @@ class CmsTest < Minitest::Test
 
   def test_view_new_document_form
     get "/new"
-
     assert_equal 200, last_response.status
     assert_includes last_response.body, "<input"
     assert_includes last_response.body, %q(<button type="submit")
@@ -107,9 +103,7 @@ class CmsTest < Minitest::Test
   def test_create_new_document
     post "/create", filename: "test.txt"
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-    assert_includes last_response.body, "test.txt has been created"
+    assert_equal "test.txt has been created", session[:success]
 
     get "/"
     assert_includes last_response.body, "test.txt"
@@ -123,16 +117,13 @@ class CmsTest < Minitest::Test
 
   def test_deleting_document
     create_document("test.txt")
-
     post "/test.txt/delete"
 
     assert_equal 302, last_response.status
-
-    get last_response["Location"]
-    assert_includes last_response.body, "test.txt has been deleted"
+    assert_equal "test.txt has been deleted", session[:success]
 
     get "/"
-    refute_includes last_response.body, "test.txt"
+    refute_includes last_response.body, %q(href="/test.txt")
   end
 
   def test_signin_form
@@ -145,26 +136,27 @@ class CmsTest < Minitest::Test
   def test_signin
     post "/users/signin", username: "admin", password: "secret"
     assert_equal 302, last_response.status
+    assert_equal "Welcome admin", session[:success]
 
     get last_response["Location"]
-    assert_includes last_response.body, "Welcome"
     assert_includes last_response.body, "Signed in as admin"
   end
 
   def test_signin_with_bad_credentials
     post "/users/signin", username: "guest", password: "shhhh"
     assert_equal 422, last_response.status
+    assert_nil session[:username]
     assert_includes last_response.body, "Invalid credentials"
   end
 
   def test_signout
-    post "/users/signin", username: "admin", password: "secret"
-    get last_response["Location"]
-    assert_includes last_response.body, "Welcome"
+    get "/", {}, {"rack.session" => { username: "admin" } }
+    assert_includes last_response.body, "Signed in as admin"
 
     post "/users/signout"
     get last_response["Location"]
 
+    assert_equal false, session[:username]
     assert_includes last_response.body, "You have been signed out"
     assert_includes last_response.body, "Sign In"
   end
